4619519ee0069ff71698a1e00293594f5c31690e
Leandro Ferreira <leanfj@hotmail.com>
2024-03-26 08:49:30 -0300

    feat: adiciona serviço e controlador de usuário

--- src/core/infrastructure/http/server.ts
+++ src/core/infrastructure/http/server.ts
@@ -1,13 +1,20 @@
 import InMemoryClienteRepository from '../../../modules/cliente/infrastructure/repositories/InMemoryCliente';
+import InMemoryUsuarioRepository from '../../../modules/usuario/infrastructure/repositories/InMemoryUsuario';
 import { ClienteService } from '../../../modules/cliente/application/Cliente.service';
+import { UsuarioService } from '../../../modules/usuario/application/Usuario.service';
 import { ClienteController } from '../../../modules/cliente/infrastructure/http/controllers/Cliente.controller';
+import { UsuarioController } from '../../../modules/usuario/infrastructure/http/controllers/Usuario.controller';
 import App from './app';
 import colors from 'colors/safe';
 
 void (async () => {
   const clienteService = new ClienteService(new InMemoryClienteRepository());
+  const usuarioService = new UsuarioService(new InMemoryUsuarioRepository());
 
-  const app = new App([new ClienteController(clienteService)]);
+  const app = new App([
+    new ClienteController(clienteService),
+    new UsuarioController(usuarioService),
+  ]);
 
   const server = app.listen();
 
