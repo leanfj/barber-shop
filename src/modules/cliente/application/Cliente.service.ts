0b991c59b1c1674d30a5d4d31a42fa9c9d86125e
Leandro Ferreira <leanfj@hotmail.com>
2024-03-27 21:59:46 -0300

    refactor: removidas as chamadas de métodos não utilizados na classe ClienteService

--- src/modules/cliente/application/Cliente.service.ts
+++ src/modules/cliente/application/Cliente.service.ts
@@ -13,9 +13,6 @@ export class ClienteService {
 
   constructor(readonly clienteRepository: IClienteRepository) {
     this.CadastratClienteUseCase = new CadastrarCliente(clienteRepository);
-    // this.updateClienteUseCase = new UpdateClienteUseCase(clienteRepository);
-    // this.getAllClienteUseCase = new GetAllClienteUseCase(clienteRepository);
-    // this.deleteClienteUseCase = new DeleteClienteUseCase(clienteRepository);
   }
 
   public async create(cliente: CadastrarClienteInput): Promise<Response> {
@@ -30,55 +27,4 @@ export class ClienteService {
       return left(new AppError.UnexpectedError(error));
     }
   }
-
-  // public async getAll(): Promise<Response> {
-  //   try {
-  //     const result = await this.getAllClienteUseCase.execute();
-
-  //     if (result.isLeft()) {
-  //       return left(result.value);
-  //     } else {
-  //       const clienteList = result.value.getValue();
-  //       return right(Result.ok<Cliente[] | Cliente>(clienteList));
-  //     }
-  //   } catch (error) {
-  //     return left(new AppError.UnexpectedError(error));
-  //   }
-  // }
-
-  // public async update(
-  //   cliente: ClienteInputDTO,
-  //   id: UniqueEntityID
-  // ): Promise<Response> {
-  //   try {
-  //     const validOrError = await validatorDTO(ClienteInputDTO, cliente, {skipMissingProperties: true});
-  //     if (validOrError.isLeft()) {
-  //       return left(validOrError.value);
-  //     }
-  //     const result = await this.updateClienteUseCase.execute({
-  //       ...cliente,
-  //       id: id.toString(),
-  //     });
-  //     if (result.isLeft()) {
-  //       return left(result.value);
-  //     } else {
-  //       return right(Result.ok<Cliente>(result.value.getValue()));
-  //     }
-  //   } catch (error) {
-  //     return left(new AppError.UnexpectedError(error));
-  //   }
-  // }
-
-  // public async delete(id: UniqueEntityID): Promise<Response> {
-  //   try {
-  //     const result = await this.deleteClienteUseCase.execute(id);
-  //     if (result.isLeft()) {
-  //       return left(result.value);
-  //     } else {
-  //       return right(Result.ok<Cliente>(result.value.getValue()));
-  //     }
-  //   } catch (error) {
-  //     return left(new AppError.UnexpectedError(error));
-  //   }
-  // }
 }
