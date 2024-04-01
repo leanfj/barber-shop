b388659ae6d7cd4ddd90ce276d3e59093f8636cd
Leandro Ferreira <leanfj@hotmail.com>
2024-04-01 16:53:55 -0300

    test: diciona teste para DataExpiracao

---
+++ __test__/src/core/domain/valueObjects/DataExpiracao.spec.ts
@@ -0,0 +1,10 @@
+import DataExpiracao from '../../../../../src/core/domain/valueObjects/DataExpiracao';
+
+describe('DataExpiracao', () => {
+  it('should create a new DataExpiracao', () => {
+    const dataExpiracao = DataExpiracao.setValue();
+
+    expect(dataExpiracao).toBeInstanceOf(DataExpiracao);
+    expect(dataExpiracao.getValue()).toBeGreaterThan(Date.now());
+  });
+});
